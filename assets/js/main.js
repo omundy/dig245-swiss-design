ac43534b9f5a9a8b57b4bcc622be178c9897d100
Owen Mundy <omundy@gmail.com>
2021-09-02 16:19:15 -0400

    Adds console message

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,19 @@
 
 /* javascript */
+
+var bauhaus = `
+
+Staaatliches Bauhaus (1919-1933)
+
+      ..-""T-..
+    ,'__   |   '.
+   /   N|  |     \
+  :     '  |      :
+  [      __|      ]
+  :      _I       :
+   \   ___I      /
+    '.  N]     ,'
+      "-"!..--"
+`;
+
+console.log(bauhaus);

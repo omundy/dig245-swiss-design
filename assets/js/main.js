e75d48760a7ca2930e5c2ddb8977f49f4cc5b90f
Owen Mundy <omundy@gmail.com>
2021-09-02 16:29:19 -0400

    Remove console msg

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,2 @@
 
 /* javascript */
-
-var bauhaus = `
-
-Staaatliches Bauhaus (1919-1933)
-
-      ..-""T-..
-    ,'__   |   '.
-   /   N|  |     !
-  :     '  |      :
-  [      __|      ]
-  :      _I       :
-   !   ___I      /
-    '.  N]     ,'
-      "-"!..--"
-
-`;
-
-console.log(bauhaus);
